c2f9dfc5b931664f313bec47854ec71b87f13f42
Vijay <vj.rajan@yoobeecolleges.com>
2019-11-26 13:08:56 +1300

    Finding Enemies

---
+++ FindEnemies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindEnemies : MonoBehaviour
+{
+    //public Collider;
+    public GameObject[] enemies;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            EnemySearch();
+    }
+
+    void EnemySearch()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            print("Enemy Number: " + i + " is named " + enemies[i].name);
+        }
+    }
+}
