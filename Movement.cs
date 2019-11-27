531bc7a972e0b28b55d6c18d58ca1d61a0f72d6b
Vijay <vj.rajan@yoobeecolleges.com>
2019-11-27 15:28:45 +1300

    Character Movement

---
+++ Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //public float rotateSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rotateSpeed = 10.0f; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //print(Input.GetAxis("Horizontal"));
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical"));
+    }
+}
