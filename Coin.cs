6b5220099e209b5ea1027337819b4fba20af0955
Vijay <vj.rajan@yoobeecolleges.com>
2019-11-28 14:58:05 +1300

    Coin Pick up

---
+++ Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotatespeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotatespeed,0.0f,0.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            print("Coin Picked up!");
+            Destroy(gameObject,10.0f);
+        }
+    }
+}
