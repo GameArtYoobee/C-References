3a7a84aaef9d48d6682e13041d1ebcd623ff95ab
Vijay <vj.rajan@yoobeecolleges.com>
2019-11-28 14:16:24 +1300

    OnTrigger

---
+++ CollissionandTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollissionandTrigger : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + "has Entered!");
+         }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name + "has Existed!");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.name + "has Stayed!");
+    }
+
+    
+}
