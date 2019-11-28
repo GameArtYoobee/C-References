28e6e388bc22f7740e87cf98435d5b0a9b11943c
Coyroski <43688077+Coyroski@users.noreply.github.com>
2019-11-28 15:26:01 +1300

    Create NewBehaviourScript.cs
    Respawn script.

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform startingPoint;
+    void Update()
+    {
+
+    }
+       
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Destroy(player);
+        player.transform.position = startingPoint.transform.position;
+    }
+}
